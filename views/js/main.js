d9be032d3e337e8f5300bce46d8d1c7db1f0e602
Dastanus <nixe141417@gmail.com>
2019-05-19 17:39:19 +0300

    some fix

--- views/js/main.js
+++ views/js/main.js
@@ -508,7 +508,7 @@ function updatePositions() {
     // document.body.scrollTop is no longer supported in Chrome.
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     var phase = Math.sin((scrollTop / 1250) + (i % 5));
-    items[i].style.transform = `translateX(${items[i].basicLeft + 100 * phase + 'px'})`;
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
